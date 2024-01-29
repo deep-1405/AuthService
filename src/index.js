a5f629ae0eed520caf8fa45e0ac1d6b31c37ac56
deep-1405 <dabhideep44@gmail.com>
2024-01-29 17:49:04 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const {User} = require('./models/index');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('./repository/user-repository');
+const UserService = require('./services/user-service');
 
 const app = express();
 
@@ -19,13 +17,13 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
-        // const repo = new UserRepository();
-        // const response = await repo.getById(1);
-        // console.log(response);
-        // const incomingpassword = '123456';
-        // const user = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
-        // console.log(response);
+        
+        const service = new UserService();
+        const newToken = service.createToken({email: 'deep@admin.com', id: 1});
+        console.log("new token is", newToken);
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRlZXBAYWRtaW4uY29tIiwiaWQiOjEsImlhdCI6MTcwNjUzMDI5NSwiZXhwIjoxNzA2NjE2Njk1fQ.NkrNMxBMNifzaiIU-3w3Lc8PHJepesoBALFBg9YEPUU';
+        const response = service.verifyToken(token);
+        console.log(response);
     });
 }   
 
