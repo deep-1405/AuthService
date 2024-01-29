4aabf5f6a19af686ff1483ce6f95b09e8907d7bc
deep-1405 <dabhideep44@gmail.com>
2024-01-29 14:58:01 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+
+// const {User} = require('./models/index');
+// const bcrypt = require('bcrypt');
+// const UserRepository = require('./repository/user-repository');
+
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -14,6 +19,13 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on Port: ${PORT}`);
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
+        // const incomingpassword = '123456';
+        // const user = await User.findByPk(3);
+        // const response = bcrypt.compareSync(incomingpassword, user.password);
+        // console.log(response);
     });
 }   
 
